f3dbb4141595360db857c164d87adc0944eb9999
Vanessa Corlette <nessacor16@gmail.com>
2019-08-08 00:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = "less than" 40
+var height = console.log(height-40)
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
